bcec8e83b3f3ef5f1b5e1b86a00bad0f374f9681
santhi550 <santhikiran0802@gmail.com>
2020-07-10 12:20:08 +0530

    demo

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -19,5 +19,8 @@ urlpatterns = [
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
     path('save_push',hello.views.save_push,name="save_push"),
+    path('login',hello.views.login,name="login"),
+    path('signup',hello.views.signup,name="signup"),
+    path('logout',hello.views.logout,name="logout"),
     path('pushbots-worker.js', (TemplateView.as_view(template_name="sw.js",content_type='application/javascript', )), name='pushbots-worker.js'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from push_notifications.models import WebPushDevice 
 from .models import Greeting
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 import json
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
-
+    if request.session.has_key('user'):
+        username = request.session['user']
+        return render(request, 'index.html', {"username" : username})
+    else:
+        return render(request, 'index.html')
 
 def db(request):
 
@@ -23,5 +27,40 @@ def db(request):
 @csrf_exempt
 def save_push(request):
     post_data=json.loads(request.body.decode('utf-8'))
-    WebPushDevice.objects.create(name=post_data.get('name'),registration_id=post_data.get('registration_id'),p256dh=post_data.get('p256dh'),active=True,auth=post_data.get('auth'),browser='Chrome')
+    WebPushDevice.objects.create(name=post_data.get('name'),registration_id=post_data.get('registration_id'),p256dh=post_data.get('p256dh'),active=True,auth=post_data.get('auth'),browser='CHROME')
     return HttpResponse("done")
+def login(request):
+    if request.method =='GET':
+        return render(request,'login.html')
+    if request.method == 'POST':
+        username=request.POST["username"]
+        pwd=request.POST["pwd"]
+        user=auth.authenticate(username=username,password=pwd)
+        if user is not None:
+            request.session['user'] = username
+            auth.login(request,user)
+            return HttpResponseRedirect('/')
+        else:
+            messages.info(request,'Invalid Credentials')
+            return  HttpResponseRedirect('/login')
+def signup(request):
+    if request.method =='GET':
+        return render(request,"signup.html")
+    if request.method=='POST':
+        email=request.POST["email"]
+        psw=request.POST["psw"]
+        user=User.objects.filter(username=email)
+        if user is not None:
+            messages.info(request,'User Already Exisits')
+            return HttpResponseRedirect('/signup')
+        else:
+            user=User.objects.create_user(username=email,password=psw,email=email)
+            user.save()
+            return  HttpResponseRedirect('/login')
+def logout(request):
+    try:
+        del request.session['user']
+    except:
+        pass
+    auth.logout(request)
+    return  HttpResponseRedirect('/')
