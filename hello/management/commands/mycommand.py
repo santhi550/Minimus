c678953e878a7ccfb0439bbcf3c0f14f6c66255c
santhi550 <santhikiran0802@gmail.com>
2020-07-11 23:17:52 +0530

    demo

--- hello/management/commands/mycommand.py
+++ hello/management/commands/mycommand.py
@@ -12,36 +12,7 @@ import types
 # headers = {
 # "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 # }
-
-# function to check if the price has dropped below 20,000
-def check_price(soup,amount,user_id,user_availability):
-  # title = soup.find(id= "productTitle").get_text()
-  pre=soup.find('span',id = "priceblock_ourprice")
-  print(pre)
-  if pre is None:
-    pre=soup.find('span',id = "priceblock_saleprice")
-    print(pre)
-  print(pre)
-  price =pre.get_text().replace(',', '').replace('₹', '').replace(' ', '').strip()
-  #print(price)
-  availability=soup.find(id="availability").get_text().strip()
-  # print(availability)
-  #converting the string amount to float
-  converted_price = float(price[0:5])
-  wp=WebPushDevice.objects.filter(user_id=user_id,active=True)
-  if user_availability and amount ==0:
-    if availability == 'In stock.':
-      wp.send_message("Dear user, the given product is in STOCK ,so book the product as soon as possible")
-  elif user_availability== False and amount > 0:
-    if converted_price < amount:
-      wp.send_message("Dear user, price for the given product has been DECREASED , so book the product as soon as possible")
-  elif user_availability and amount > 0:
-    if availability == 'In stock.' and converted_price < amount:
-      wp.send_message("Dear user, the given product is in STOCK and its price has been DECREASED , so Book the product as soon as possible")
-
-
-def mainprogram(url,amount,user_id,availability):
-  site='https://www.amazon.in/gp/sign-in.html'
+site='https://www.amazon.in/gp/sign-in.html'
 
   session = requests.Session()
   
@@ -76,6 +47,34 @@ def mainprogram(url,amount,user_id,availability):
       print('Login Successfull')
   else:
       print('Login Failed')
+# function to check if the price has dropped below 20,000
+def check_price(soup,amount,user_id,user_availability):
+  # title = soup.find(id= "productTitle").get_text()
+  pre=soup.find('span',id = "priceblock_ourprice")
+  print(pre)
+  if pre is None:
+    pre=soup.find('span',id = "priceblock_saleprice")
+    print(pre)
+  print(pre)
+  price =pre.get_text().replace(',', '').replace('₹', '').replace(' ', '').strip()
+  #print(price)
+  availability=soup.find(id="availability").get_text().strip()
+  # print(availability)
+  #converting the string amount to float
+  converted_price = float(price[0:5])
+  wp=WebPushDevice.objects.filter(user_id=user_id,active=True)
+  if user_availability and amount ==0:
+    if availability == 'In stock.':
+      wp.send_message("Dear user, the given product is in STOCK ,so book the product as soon as possible")
+  elif user_availability== False and amount > 0:
+    if converted_price < amount:
+      wp.send_message("Dear user, price for the given product has been DECREASED , so book the product as soon as possible")
+  elif user_availability and amount > 0:
+    if availability == 'In stock.' and converted_price < amount:
+      wp.send_message("Dear user, the given product is in STOCK and its price has been DECREASED , so Book the product as soon as possible")
+
+
+def mainprogram(url,amount,user_id,availability):
   try:
     response = requests.get(url, headers=session.headers,timeout=600)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -84,7 +83,6 @@ def mainprogram(url,amount,user_id,availability):
     check_price(soup,amount,user_id,availability)
   except:
     pass
-  session.close()
 
 while(True):
   items_list=Items.objects.all()
@@ -92,4 +90,5 @@ while(True):
   for item in items_list:
     print(item.url)
     mainprogram(item.url,item.amount,item.user_id,item.availability) 
-  time.sleep(5)
+  time.sleep(30)
+session.close()
