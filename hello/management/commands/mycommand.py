814489d3edf10ff37ac1b9201b2dc2cdab4cf1e2
santhi550 <santhikiran0802@gmail.com>
2020-07-11 23:05:47 +0530

    demo

--- hello/management/commands/mycommand.py
+++ hello/management/commands/mycommand.py
@@ -9,43 +9,10 @@ from bs4 import BeautifulSoup
 import time
 import types
 # set the headers and user string
-site='https://www.amazon.in/gp/sign-in.html'
 # headers = {
 # "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 # }
-session = requests.Session()
- 
-'''define session headers'''
-session.headers = {
-'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Accept-Language': 'en-US,en;q=0.5',
-'Referer': site
-}
 
-resp = session.get(site)
-html = resp.text
- 
-'''get BeautifulSoup object of the html of the login page'''
-soup = BeautifulSoup(html , 'lxml')
-data = {}
-form = soup.find('form', {'name': 'signIn'})
-for field in form.find_all('input'):
-  try:
-      data[field['name']] = field['value']
-  
-  except:
-      pass
-data[u'email'] = 'santhikiran2000@gmail.com'
-data[u'password'] = 'spring@123'
-post_resp = session.post('https://www.amazon.in/ap/signin', data = data)
-#print(soup.prettify())
-post_soup = BeautifulSoup(post_resp.content , 'lxml')
- 
-if post_soup.find_all('title')[0].text == 'Your Account':
-    print('Login Successfull')
-else:
-    print('Login Failed')
 # function to check if the price has dropped below 20,000
 def check_price(soup,amount,user_id,user_availability):
   # title = soup.find(id= "productTitle").get_text()
@@ -74,6 +41,41 @@ def check_price(soup,amount,user_id,user_availability):
 
 
 def mainprogram(url,amount,user_id,availability):
+  site='https://www.amazon.in/gp/sign-in.html'
+
+  session = requests.Session()
+  
+  '''define session headers'''
+  session.headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Language': 'en-US,en;q=0.5',
+  'Referer': site
+  }
+
+  resp = session.get(site)
+  html = resp.text
+  
+  '''get BeautifulSoup object of the html of the login page'''
+  soup = BeautifulSoup(html , 'lxml')
+  data = {}
+  form = soup.find('form', {'name': 'signIn'})
+  for field in form.find_all('input'):
+    try:
+        data[field['name']] = field['value']
+    
+    except:
+        pass
+  data[u'email'] = 'santhikiran2000@gmail.com'
+  data[u'password'] = 'spring@123'
+  post_resp = session.post('https://www.amazon.in/ap/signin', data = data)
+  #print(soup.prettify())
+  post_soup = BeautifulSoup(post_resp.content , 'lxml')
+  
+  if post_soup.find_all('title')[0].text == 'Your Account':
+      print('Login Successfull')
+  else:
+      print('Login Failed')
   try:
     response = requests.get(url, headers=session.headers,timeout=600)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -82,6 +84,7 @@ def mainprogram(url,amount,user_id,availability):
     check_price(soup,amount,user_id,availability)
   except:
     pass
+  session.close()
 
 while(True):
   items_list=Items.objects.all()
@@ -90,4 +93,3 @@ while(True):
     print(item.url)
     mainprogram(item.url,item.amount,item.user_id,item.availability) 
   time.sleep(5)
-session.close()
