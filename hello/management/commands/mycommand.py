2de1ab25ce1ca2c47119646115923264666e4b11
santhi550 <santhikiran0802@gmail.com>
2020-07-11 20:04:52 +0530

    demo

--- hello/management/commands/mycommand.py
+++ hello/management/commands/mycommand.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand, CommandError
 import schedule 
 from push_notifications.models import WebPushDevice 
 from hello.models import Items
-import grequests
+import requests
 from bs4 import BeautifulSoup
 import time
 import types
@@ -45,7 +45,7 @@ def check_price(soup,amount,user_id,user_availability):
 
 def mainprogram(url,amount,user_id,availability):
   try:
-    response = grequests.get(url, headers=headers)
+    response = requests.get(url, headers=headers,timeout=60)
     soup = BeautifulSoup(response.content, 'html.parser')
     print(response)
     soup.encode('utf-8')
